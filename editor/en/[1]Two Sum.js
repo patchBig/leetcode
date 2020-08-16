1997d970e94285895f0567420f6571f4468e905a
nicalyang <1843943831@qq.com>
2020-08-16 15:21:01 +0800

    :tada: 初始化

---
+++ editor/en/[1]Two Sum.js
@@ -0,0 +1,35 @@
+//Given an array of integers, return indices of the two numbers such that they a
+//dd up to a specific target. 
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice. 
+//
+// Example: 
+//
+// 
+//Given nums = [2, 7, 11, 15], target = 9,
+//
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+// 
+// Related Topics Array Hash Table 
+// 👍 16182 👎 586
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0, j = nums.length; i < j; i++) {
+        for(let k = i + 1; k < j; k++ ) {
+            if (nums[i] + nums[k] === target) {
+                return [i, k]
+            }
+        }
+    }
+};
+console.log(twoSum([2, 7, 11, 15], 9))
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/en/[41]First Missing Positive.js
@@ -0,0 +1,40 @@
+//Given an unsorted integer array, find the smallest missing positive integer. 
+//
+// Example 1: 
+//
+// 
+//Input: [1,2,0]
+//Output: 3
+// 
+//
+// Example 2: 
+//
+// 
+//Input: [3,4,-1,1]
+//Output: 2
+// 
+//
+// Example 3: 
+//
+// 
+//Input: [7,8,9,11,12]
+//Output: 1
+// 
+//
+// Follow up: 
+//
+// Your algorithm should run in O(n) time and uses constant extra space. 
+// Related Topics Array 
+// 👍 3768 👎 817
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let num = nums.filter(n => n >= 1);
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
